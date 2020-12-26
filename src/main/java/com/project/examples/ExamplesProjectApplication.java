5fbf0a2b3028935880a5457c1de2430d5af2b581
lavelles <Shaun.Lavelle@utiligroup.com>
2020-12-26 22:00:21 +0000

    Initial commit

---
+++ src/main/java/com/project/examples/ExamplesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/examples/ExamplesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
